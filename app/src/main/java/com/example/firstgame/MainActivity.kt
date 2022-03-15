07d41821d12cc4a6268a4c3902745e8219569cc2
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-03-15 13:45:09 +0500

    functionality added

--- app/src/main/java/com/example/firstgame/MainActivity.kt
+++ app/src/main/java/com/example/firstgame/MainActivity.kt
@@ -14,7 +14,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        var userNum = findViewById<EditText>(R.id.editText)
+        var message = findViewById<TextView>(R.id.textView)
+        var btn = findViewById<Button>(R.id.button)
+        var computerNum = findViewById<TextView>(R.id.textView2)
 
+        btn.setOnClickListener(){
+            var guessedNum = Integer.parseInt((userNum.text).toString())
+            var randNum = (1..100).random()
+            if(guessedNum < randNum){
+                message.setText("Your Guess was less than computer guess")
+            }
+            else if(guessedNum > randNum){
+                message.setText("Your Guess was greater than computer guess")
+            }
+            else{
+                message.setText("Wow! You Guessed Correct!")
+            }
+            computerNum.setText(randNum.toString())
+        }
 
     }
 }
