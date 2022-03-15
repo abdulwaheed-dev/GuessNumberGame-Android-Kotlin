8cadce0fc5e019c1ef24b722c1f2a5dddba3a3f2
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-03-15 13:39:17 +0500

    Design commit

---
+++ app/src/main/java/com/example/firstgame/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.firstgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
